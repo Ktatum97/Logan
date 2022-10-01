b436c9ced9848f8563703fff53421b09e582e9d4
Kevin <kevin.tatum.hidalgo@utelvt.edu.ec>
2022-10-01 19:13:11 +0000

    cambié las variables de contabilización

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -1,20 +1,20 @@
-//Creado por: Kevin Logan Tatum Hidalgo
+//Creado por: Kevin Logan Tatum Hidalgo 
 #include<iostream>
 using namespace std;
 int main()
 {
-	int i=0,l;
-	float x,s=0;
-	cout<<"ingrese el limite l="; cin>>l;
+	int KLTH_i=0,KLTH_l;
+	float KLTH_x,KLTH_s=0;
+	cout<<"ingrese el limite KLTH_l="; cin>>KLTH_l;
 	do{
 
-	cout<<"ingrese el numero x="; cin>>x;
-	i=i+1;
-	s=s+x;
+	cout<<"ingrese el numero KLTH_x="; cin>>KLTH_x;
+	KLTH_i=KLTH_i+1;
+	KLTH_s=KLTH_s+KLTH_x;
 
 
 
-	}while(i<l);
-	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
+	}while(KLTH_i<KLTH_l);
+	cout<<"Se ingresaron "<<KLTH_l<<" numeros "<< "que sumaron "<<KLTH_s<<endl;
 	return 0;
 }
